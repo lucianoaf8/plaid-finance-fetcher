b63ebb161c0eac2189f874e536c8d9fbdc43f581
Luciano A <lucianoaf8@gmail.com>
2024-06-16 13:52:49 -0600

    Added import tranactions

--- data/database/create tables.sql
+++ data/database/create tables.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS finance.file_import_tracker;
-CREATE TABLE finance.file_import_tracker (
+USE finance;
+
+DROP TABLE IF EXISTS file_import_tracker;
+CREATE TABLE file_import_tracker (
     id INT AUTO_INCREMENT PRIMARY KEY,
     file_name VARCHAR(255) NOT NULL,
     import_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -21,9 +23,8 @@ CREATE TABLE finance.plaid_liabilities_credit (
     FOREIGN KEY (file_import_id) REFERENCES file_import_tracker(id) ON DELETE CASCADE
 );
 
-
-DROP TABLE IF EXISTS finance.plaid_liabilities_credit_apr;
-CREATE TABLE finance.plaid_liabilities_credit_apr (
+DROP TABLE IF EXISTS plaid_liabilities_credit_apr;
+CREATE TABLE plaid_liabilities_credit_apr (
     id INT AUTO_INCREMENT PRIMARY KEY,
     account_id VARCHAR(255) NOT NULL,
     apr_percentage DECIMAL(5, 2),
@@ -33,4 +34,69 @@ CREATE TABLE finance.plaid_liabilities_credit_apr (
     file_import_id INT,
     FOREIGN KEY (file_import_id) REFERENCES file_import_tracker(id) ON DELETE CASCADE,
     FOREIGN KEY (account_id) REFERENCES plaid_liabilities_credit(account_id)
+);
+
+DROP TABLE IF EXISTS plaid_transactions;
+CREATE TABLE plaid_transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    account_id VARCHAR(255) NOT NULL,
+    transaction_id VARCHAR(255) NOT NULL UNIQUE,
+    account_owner VARCHAR(255),
+    amount DECIMAL(10, 2),
+    authorized_date DATE,
+    authorized_datetime DATETIME,
+    date DATE,
+    datetime DATETIME,
+    iso_currency_code VARCHAR(10),
+    logo_url VARCHAR(255),
+    merchant_entity_id VARCHAR(255),
+    merchant_name VARCHAR(255),
+    name VARCHAR(255),
+    payment_channel VARCHAR(50),
+    pending BOOLEAN,
+    pending_transaction_id VARCHAR(255),
+    transaction_code VARCHAR(255),
+    transaction_type VARCHAR(50),
+    unofficial_currency_code VARCHAR(10),
+    category VARCHAR(255),
+    category_id VARCHAR(255),
+    personal_finance_category_confidence_level VARCHAR(50),
+    personal_finance_category_detailed VARCHAR(255),
+    personal_finance_category_primary VARCHAR(255),
+    personal_finance_category_icon_url VARCHAR(255),
+    location_address VARCHAR(255),
+    location_city VARCHAR(255),
+    location_region VARCHAR(50),
+    location_postal_code VARCHAR(20),
+    location_country VARCHAR(50),
+    location_lat DECIMAL(10, 7),
+    location_lon DECIMAL(10, 7),
+    location_store_number VARCHAR(50),
+    payment_meta_reference_number VARCHAR(255),
+    payment_meta_ppd_id VARCHAR(255),
+    payment_meta_payee VARCHAR(255),
+    payment_meta_by_order_of VARCHAR(255),
+    payment_meta_payer VARCHAR(255),
+    payment_meta_payment_method VARCHAR(255),
+    payment_meta_payment_processor VARCHAR(255),
+    payment_meta_reason VARCHAR(255),
+    website VARCHAR(255),
+    check_number VARCHAR(255),
+    file_import_id INT,
+    FOREIGN KEY (file_import_id) REFERENCES file_import_tracker(id) ON DELETE CASCADE
+);
+
+
+DROP TABLE IF EXISTS plaid_transaction_counterparties;
+CREATE TABLE plaid_transaction_counterparties (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    transaction_id VARCHAR(255),
+    name VARCHAR(255),
+    type VARCHAR(50),
+    website VARCHAR(255),
+    logo_url VARCHAR(255),
+    confidence_level VARCHAR(50),
+    entity_id VARCHAR(255),
+    phone_number VARCHAR(50),
+    FOREIGN KEY (transaction_id) REFERENCES plaid_transactions(transaction_id) ON DELETE CASCADE
 );
--- insert_data.py
+++ insert_data.py
@@ -45,6 +45,15 @@ def is_file_imported(file_name):
     conn.close()
     return result > 0
 
+def account_exists(account_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM plaid_liabilities_credit WHERE account_id = %s", (account_id,))
+    result = cursor.fetchone()[0]
+    cursor.close()
+    conn.close()
+    return result > 0
+
 def insert_liabilities(data, bank_name, file_name):
     if is_file_imported(file_name):
         message = f"File {file_name} has already been imported. Skipping..."
@@ -69,6 +78,12 @@ def insert_liabilities(data, bank_name, file_name):
 
         for credit in data['credit']:
             account_id = credit['account_id']
+            if account_exists(account_id):
+                message = f"Account ID {account_id} already exists. Skipping insertion."
+                print(message)
+                logging.info(message)
+                continue
+
             is_overdue = credit['is_overdue']
             last_payment_amount = credit['last_payment_amount']
             last_payment_date = credit['last_payment_date']
@@ -119,20 +134,119 @@ def insert_liabilities(data, bank_name, file_name):
         cursor.close()
         conn.close()
 
-def import_liabilities_files():
+def insert_transactions(data, bank_name, file_name):
+    if is_file_imported(file_name):
+        message = f"File {file_name} has already been imported. Skipping..."
+        print(message)
+        logging.info(message)
+        return
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        description = f"Transactions data for {bank_name} fetched at {timestamp}"
+
+        # Insert record in file_import_tracker
+        cursor.execute("""
+            INSERT INTO file_import_tracker (file_name, description) 
+            VALUES (%s, %s)
+        """, (file_name, description))
+
+        tracker_id = cursor.lastrowid
+
+        for transaction in data:
+            params = (
+                transaction['account_id'], transaction['transaction_id'], transaction.get('account_owner'), transaction['amount'],
+                transaction.get('authorized_date'), transaction.get('authorized_datetime'), transaction['date'], transaction.get('datetime'),
+                transaction['iso_currency_code'], transaction.get('logo_url'), transaction.get('merchant_entity_id'), transaction.get('merchant_name'),
+                transaction['name'], transaction['payment_channel'], transaction['pending'], transaction.get('pending_transaction_id'),
+                transaction.get('transaction_code'), transaction['transaction_type'], transaction.get('unofficial_currency_code'),
+                ", ".join(transaction['category']), transaction['category_id'], transaction['personal_finance_category'].get('confidence_level'),
+                transaction['personal_finance_category'].get('detailed'), transaction['personal_finance_category'].get('primary'),
+                transaction['personal_finance_category_icon_url'], transaction['location'].get('address'), transaction['location'].get('city'),
+                transaction['location'].get('region'), transaction['location'].get('postal_code'), transaction['location'].get('country'),
+                transaction['location'].get('lat'), transaction['location'].get('lon'), transaction['location'].get('store_number'),
+                transaction['payment_meta'].get('reference_number'), transaction['payment_meta'].get('ppd_id'),
+                transaction['payment_meta'].get('payee'), transaction['payment_meta'].get('by_order_of'), transaction['payment_meta'].get('payer'),
+                transaction['payment_meta'].get('payment_method'), transaction['payment_meta'].get('payment_processor'),
+                transaction['payment_meta'].get('reason'), transaction.get('website'), transaction.get('check_number'), tracker_id
+            )
+            
+            cursor.execute("""
+                INSERT INTO plaid_transactions (
+                    account_id, transaction_id, account_owner, amount, 
+                    authorized_date, authorized_datetime, date, datetime, 
+                    iso_currency_code, logo_url, merchant_entity_id, merchant_name, 
+                    name, payment_channel, pending, pending_transaction_id, 
+                    transaction_code, transaction_type, unofficial_currency_code, 
+                    category, category_id, personal_finance_category_confidence_level, 
+                    personal_finance_category_detailed, personal_finance_category_primary, 
+                    personal_finance_category_icon_url, location_address, 
+                    location_city, location_region, location_postal_code, 
+                    location_country, location_lat, location_lon, location_store_number, 
+                    payment_meta_reference_number, payment_meta_ppd_id, 
+                    payment_meta_payee, payment_meta_by_order_of, payment_meta_payer, 
+                    payment_meta_payment_method, payment_meta_payment_processor, 
+                    payment_meta_reason, website, check_number, file_import_id
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, params)
+
+            for counterparty in transaction['counterparties']:
+                cursor.execute("""
+                    INSERT INTO plaid_transaction_counterparties (
+                        transaction_id, name, type, website, logo_url, 
+                        confidence_level, entity_id, phone_number
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                """, (
+                    transaction['transaction_id'], counterparty.get('name'), counterparty.get('type'), counterparty.get('website'),
+                    counterparty.get('logo_url'), counterparty.get('confidence_level'), counterparty.get('entity_id'), counterparty.get('phone_number')
+                ))
+
+        conn.commit()
+        message = f"Successfully inserted transactions for {bank_name} from {file_name}"
+        print(message)
+        logging.info(message)
+    except Exception as e:
+        message = f"Error inserting transactions for {bank_name} from {file_name}: {e}"
+        print(message)
+        logging.error(message)
+    finally:
+        cursor.close()
+        conn.close()
+
+def import_files():
     fetched_files_dir = 'data/fetched-files/'
     for file_name in os.listdir(fetched_files_dir):
         if file_name.startswith("plaid_liabilities_") and file_name.endswith(".json"):
             print(f"Processing file: {file_name}")
             logging.info(f"Processing file: {file_name}")
-            with open(fetched_files_dir + file_name) as file:
-                data = json.load(file)
-                bank_name = file_name.split("_")[2].replace(".json", "")
-                insert_liabilities(data, bank_name, file_name)
+            try:
+                with open(fetched_files_dir + file_name) as file:
+                    data = json.load(file)
+                    bank_name = file_name.split("_")[2].replace(".json", "")
+                    # insert_liabilities(data, bank_name, file_name)
+            except json.JSONDecodeError as e:
+                message = f"Error decoding JSON from file {file_name}: {e}"
+                print(message)
+                logging.error(message)
+        elif file_name.startswith("plaid_transactions_") and file_name.endswith(".json"):
+            print(f"Processing file: {file_name}")
+            logging.info(f"Processing file: {file_name}")
+            try:
+                with open(fetched_files_dir + file_name) as file:
+                    data = json.load(file)
+                    bank_name = file_name.split("_")[2].replace(".json", "")
+                    insert_transactions(data, bank_name, file_name)
+            except json.JSONDecodeError as e:
+                message = f"Error decoding JSON from file {file_name}: {e}"
+                print(message)
+                logging.error(message)
 
 if __name__ == "__main__":
     print("Starting import process...")
     logging.info("Starting import process...")
-    import_liabilities_files()
+    import_files()
     print("Import process completed.")
     logging.info("Import process completed.")
